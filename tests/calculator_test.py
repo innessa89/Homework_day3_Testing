891f17a3e1eb6206fd1bcb804fb7d6e2e27d1014
innessa89 <innessa89@gmail.com>
2021-10-13 23:47:28 +0100

    Test code

---
+++ tests/calculator_test.py
@@ -0,0 +1,17 @@
+import unittest
+from src.calculator import add, divide, multiply, subtract
+class TestCalculator(unittest.TestCase):
+    def test_add(self): # NEW
+        self.assertEqual(5, add(2, 3))
+
+
+    def test_subtract(self):
+        self.assertEqual(3, subtract(10, 7))
+
+
+    def test_divide(self):
+        self.assertEqual(5, divide(10, 2))    
+
+
+    def test_multiply(self):
+        self.assertEqual(16, multiply(8, 2))   
